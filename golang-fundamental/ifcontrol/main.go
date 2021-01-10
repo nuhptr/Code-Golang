33ae012f8632ad4697b22c7cbb8455d6d5aa6162
nuhptr <nugrahaadi733@gmail.com>
2021-01-10 17:15:49 +0700

    if control

---
+++ golang-fundamental/ifcontrol/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// if else
+	var age int
+	age = 10
+	if age > 10  {
+		fmt.Println("Kamu Boleh bermain game")
+	} else {
+		// execute
+		fmt.Println("Tidak Boleh bermain Game")
+	}
+
+	// elseif
+	score := 80
+	var grade string
+
+	if score <= 50  {
+		grade = "E"
+	}else if score <= 60 {
+		grade = "D"
+	} else if score <= 70 {
+		grade = "C"
+	} else {
+		// execute
+		grade = "Null"
+	}
+
+	fmt.Println(grade)
+}
+
---
+++ golang-fundamental/variabel/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Adi Nugraha Putra"
+	fmt.Println(name) // Adi Nugraha Putra
+}
